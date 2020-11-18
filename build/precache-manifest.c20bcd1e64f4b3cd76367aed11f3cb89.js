15e9b04b684b7044ae8260abd85539d9ca91c0a5
Vriska Weaver <weavriska@gmail.com>
2020-11-17 20:13:05 -0500

    initial build subtree add

---
+++ build/precache-manifest.c20bcd1e64f4b3cd76367aed11f3cb89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "ec6ef68268a11e7229b24610fcb746ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "ce33df28f7a72ec10724",
+    "url": "/static/css/main.e003d7e0.chunk.css"
+  },
+  {
+    "revision": "ce33df28f7a72ec10724",
+    "url": "/static/js/main.ce33df28.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
