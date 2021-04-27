a40e65ad06140158f319000a226139fdb6fb817d
henlo-birb <weavriska@gmail.com>
2021-04-27 17:35:38 -0400

    Updates

---
+++ precache-manifest.40a86f49c88fe7f18675d58a927eb2db.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "f2420d4eefd7b2880d6780e18537f4ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2192e9b000f464fc69d31caed0f6ba54",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "b4c3b56a7391efbcc8f1b15a90906d1a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "c845336ce13c6a567484",
+    "url": "/static/css/main.ae214e61.chunk.css"
+  },
+  {
+    "revision": "c845336ce13c6a567484",
+    "url": "/static/js/main.c845336c.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
